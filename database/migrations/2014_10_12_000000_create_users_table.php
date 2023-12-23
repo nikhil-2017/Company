173479db5e5b16d990acdacaa8ec553ab1b24166
Nikhil Gaikwad <nickgaikwad11@gmail.com>
2023-12-24 02:24:46 +0530

    [Issue #2] Laravel app.

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid')->unique();
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->softDeletes();
+            $table->boolean('disabled')->default(false);
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+            $table->string('company')->nullable();
+            $table->string('phone_number')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
